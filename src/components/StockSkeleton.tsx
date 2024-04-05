e35b9a9d2a1eb516f44be19a5e502ff96c4d25da
rd273001 <rd273001@gmail.com>
2024-04-06 02:17:00 +0530

    Updated Skeleton Loading UI

--- src/components/StockSkeleton.tsx
+++ src/components/StockSkeleton.tsx
@@ -4,19 +4,19 @@ import 'react-loading-skeleton/dist/skeleton.css';
 
 const StockSkeleton: React.FC = () => {
   return (
-    <SkeletonTheme baseColor="#d9d9d9" highlightColor="#e9e9e9">
+    <SkeletonTheme baseColor="#c9c9c9" highlightColor="#e9e9e9">
         <table className="table-auto sm:w-1/2 w-full mt-10 sm:mt-20 mx-auto text-center">
           <thead>
             <tr>
-              <th className="border-4 border-gray-700 px-4 py-2"><Skeleton width={ 80 } /></th>
-              <th className="border-4 border-gray-700 px-4 py-2"><Skeleton width={ 80 } /></th>
-              <th className="border-4 border-gray-700 px-4 py-2"><Skeleton width={ 80 } /></th>
+              <th className="border-4 border-gray-700 px-4 py-2">Date</th>
+              <th className="border-4 border-gray-700 px-4 py-2">Open</th>
+              <th className="border-4 border-gray-700 px-4 py-2">Price</th>
             </tr>
           </thead>
           <tbody>
             { [...Array( 3 )].map( ( _, index ) => (
               <tr key={ index }>
-                <td className="border-4 border-gray-700 px-4 py-2"><Skeleton width={ 80 } /></td>
+                <td className="border-4 border-gray-700 px-4 py-2"><Skeleton width={ 100 } /></td>
                 <td className="border-4 border-gray-700 px-4 py-2"><Skeleton width={ 80 } /></td>
                 <td className="border-4 border-gray-700 px-4 py-2"><Skeleton width={ 80 } /></td>
               </tr>
